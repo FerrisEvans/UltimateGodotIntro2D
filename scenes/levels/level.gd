beee34116cb8c46512e49fa6c44ed33f595ae9ea
Ferris <ferris.evans@outlook.com>
2025-02-16 13:17:44 +0800

    custom signal

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+func _on_gate_player_entered_gate() -> void:
+	print("maybe receive the signal")
+
+func _on_player_laser() -> void:
+	print("laser from level")
+
+func _on_player_grenade() -> void:
+	print("grenade from level")
--- scenes/players/player.gd
+++ scenes/players/player.gd
@@ -1,24 +1,10 @@
 extends CharacterBody2D
 
+signal laser
+signal grenade
+
 var can_laser: bool = true
 var can_grenade: bool = true
-
-func _process(_delta: float) -> void:
-	# input
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * 500
-	move_and_slide()
-	
-	# laser shooting input
-	if can_laser && Input.is_action_pressed("primary action"):
-		print("shoot laser")
-		can_laser = false
-		$LaserReloadTimer.start()
-
-	if can_grenade and Input.is_action_pressed("secondary action"):
-		print("shoot grenade")
-		can_grenade = false
-		$GrenadeReloadTimer.start(0.5)
 
 func _on_timer_timeout() -> void:
 	if not can_laser:
@@ -29,3 +15,20 @@ func _on_grenade_reload_timer_timeout() -> void:
 	if not can_grenade:
 		can_grenade = true
 		print("can grenade")
+
+func _process(_delta: float) -> void:
+	# input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
+	
+	# laser shooting input
+	if can_laser && Input.is_action_pressed("primary action"):
+		laser.emit()
+		can_laser = false
+		$LaserReloadTimer.start()
+
+	if can_grenade and Input.is_action_pressed("secondary action"):
+		grenade.emit()
+		can_grenade = false
+		$GrenadeReloadTimer.start()

5acd9d80a0692abfe8afb87dcf31ba154153309a
Ferris <ferris.evans@outlook.com>
2025-02-16 16:13:37 +0800

    laser & grenade

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
-func _on_gate_player_entered_gate() -> void:
-	print("maybe receive the signal")
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 
-func _on_player_laser() -> void:
-	print("laser from level")
+func _on_gate_player_entered_gate(body) -> void:
+	print(body)
 
-func _on_player_grenade() -> void:
-	print("grenade from level")
+func _on_player_laser(pos) -> void:
+	var laser = laser_scene.instantiate() as Area2D
+	# 1. update the laser position
+	laser.position = pos
+	# 2. we have to move the laser
+	# 3. add the laser instance to a Node2D
+	$Projectiles.add_child(laser)
+
+func _on_player_grenade(pos) -> void:
+	var grenade = grenade_scene.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.linear_velocity = Vector2.UP * 100
+	$Projectiles.add_child(grenade)
--- scenes/players/player.gd
+++ scenes/players/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser
-signal grenade
+signal laser(pos)
+signal grenade(pos)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -9,12 +9,10 @@ var can_grenade: bool = true
 func _on_timer_timeout() -> void:
 	if not can_laser:
 		can_laser = true
-		print("can laser")
 
 func _on_grenade_reload_timer_timeout() -> void:
 	if not can_grenade:
 		can_grenade = true
-		print("can grenade")
 
 func _process(_delta: float) -> void:
 	# input
@@ -24,11 +22,15 @@ func _process(_delta: float) -> void:
 	
 	# laser shooting input
 	if can_laser && Input.is_action_pressed("primary action"):
-		laser.emit()
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		laser.emit(selected_laser.global_position)
 		can_laser = false
 		$LaserReloadTimer.start()
 
 	if can_grenade and Input.is_action_pressed("secondary action"):
-		grenade.emit()
+		var grenade_markers = $GrenadeStartPosition.get_children()
+		var selected_grenade = grenade_markers[randi() % grenade_markers.size()]
+		grenade.emit(selected_grenade.global_position)
 		can_grenade = false
 		$GrenadeReloadTimer.start()

cf3f151853dd312893924f0c0335364d803fa383
Ferris <ferris.evans@outlook.com>
2025-03-12 16:29:09 +0800

    transmition

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -8,4 +8,5 @@ func _on_exit_gate_area_body_entered(_body: Node2D) -> void:
 	tween.tween_property($Player, 'speed', 0, 1)
 	# get_tree().change_scene_to_file('res://scenes/levels/outside.tscn')
 	# get_tree().change_scene_to_packed(outside_level_scene)
-	get_tree().call_deferred('change_scene_to_packed', outside_level_scene)
+	# get_tree().call_deferred('change_scene_to_packed', outside_level_scene)
+	TransitionLayer.change_scene('res://scenes/levels/outside.tscn')
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -5,4 +5,5 @@ func _on_gate_player_entered_gate(_body: Variant) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, 'speed', 0, 1)
 	# get_tree().change_scene_to_file('res://scenes/levels/inside.tscn')
-	get_tree().call_deferred('change_scene_to_file', 'res://scenes/levels/inside.tscn')
+	# get_tree().call_deferred('change_scene_to_file', 'res://scenes/levels/inside.tscn')
+	TransitionLayer.change_scene('res://scenes/levels/inside.tscn')

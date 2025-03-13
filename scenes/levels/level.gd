4601bcb005912c25b97fc230d4fab5c308e93ffd
Ferris <ferris.evans@outlook.com>
2025-03-13 22:23:44 +0800

    light

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -14,7 +14,6 @@ func _on_player_laser(pos, direction: Vector2) -> void:
 	
 	# 2. we have to move the laser
 	# 3. add the laser instance to a Node2D
-	$UI.update_laser_text()
 	$Projectiles.add_child(laser)
 
 
@@ -22,18 +21,7 @@ func _on_player_grenade(pos, direction) -> void:
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
-	$UI.update_grenade_text()
 	$Projectiles.add_child(grenade)
-
-
-func _on_house_player_entered() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, 'zoom', Vector2(1, 1), 1)
-
-
-func _on_house_player_left() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, 'zoom', Vector2(0.6, 0.6), 1)
 
 func _on_container_open(pos, direction):
 	var item = item_scene.instantiate()
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -7,3 +7,12 @@ func _on_gate_player_entered_gate(_body: Variant) -> void:
 	# get_tree().change_scene_to_file('res://scenes/levels/inside.tscn')
 	# get_tree().call_deferred('change_scene_to_file', 'res://scenes/levels/inside.tscn')
 	TransitionLayer.change_scene('res://scenes/levels/inside.tscn')
+
+func _on_house_player_entered() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, 'zoom', Vector2(1, 1), 1)
+
+
+func _on_house_player_left() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, 'zoom', Vector2(0.6, 0.6), 1)

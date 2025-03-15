0c867572ee460de750ad12f5763ef68c96583258
Ferris <ferris.evans@outlook.com>
2025-03-15 19:46:57 +0800

    drone

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,13 +1,38 @@
 extends CharacterBody2D
 
-func _process(_delta: float):
-	# direction
-	var direction = Vector2.RIGHT
-	# velocity
-	velocity = direction * 200
-	# move and slide
-	move_and_slide()
+var active: bool = false
+var vulnerable: bool = true
+var health: int = 50
+@export var speed: int = 400
 
+
+func _ready() -> void:
+	$Drone.show()
+
+func _process(delta: float):
+	if active:
+		look_at(Globals.player_pos)
+		velocity = (Globals.player_pos - position).normalized() * speed
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			$AnimationPlayer.play('explosion')
+
+func stop_movement() -> void:
+	active = false
 
 func hit():
-	print('damage')
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$HitTimer.start()
+		$Drone.material.set_shader_parameter('progress', 1)
+	if health <= 0:
+		$AnimationPlayer.play('explosion')
+
+
+func _on_notice_area_body_entered(_body: Node2D) -> void:
+	active = true
+
+func _on_hit_timer_timeout() -> void:
+	vulnerable = true
+	$Drone.material.set_shader_parameter('progress', 0)
